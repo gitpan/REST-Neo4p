b74781a13a879c3111d7b50c675bdb05652e2cbc
Mark Allen Jensen <jensen@fortinbras.us>
2014-02-18 00:29:08 +0000

    Import of MAJENSEN/REST-Neo4p-0.2242 from CPAN.
    gitpan-cpan-distribution: REST-Neo4p
    gitpan-cpan-version:      0.2242
    gitpan-cpan-path:         MAJENSEN/REST-Neo4p-0.2242.tar.gz
    gitpan-cpan-author:       MAJENSEN
    gitpan-cpan-maturity:     released

--- lib/REST/Neo4p/Exceptions.pm
+++ lib/REST/Neo4p/Exceptions.pm
@@ -1,7 +1,10 @@
 #$Id: Exceptions.pm 354 2014-02-17 05:22:50Z maj $
 package REST::Neo4p::Exceptions;
+use strict;
+use warnings;
+
 BEGIN {
-  $REST::Neo4p::Exceptions::VERSION = '0.2241';
+  $REST::Neo4p::Exceptions::VERSION = '0.2242';
 }
 use Exception::Class (
   'REST::Neo4p::Exception',
